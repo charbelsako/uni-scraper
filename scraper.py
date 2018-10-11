f7cea008644654720aad2ee649640acb0e9cc28a
charbelsako <charbelsarkis82581@gmail.com>
2018-10-11 15:16:46 +0300

    the scraper can now enter the courses

--- scraper.py
+++ scraper.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions
 # SAFELY READ YOUR PASSWORD WITHOUT ANYONE SEEING IT
 import configparser
 config = configparser.ConfigParser()
@@ -12,8 +12,8 @@ password = config['SECRET']['PASSWORD']
 # start chrome
 browser = webdriver.Chrome()
 # navigate to your url
-url = 'http://elearning.aut.edu/course/view.php?id=1548'
-browser.get((url))
+url = 'http://elearning.aut.edu/my/'
+browser.get(url)
 
 # fill in username
 usernameInput = browser.find_element_by_id('username')
@@ -21,8 +21,16 @@ usernameInput.send_keys(username)
 # fill in your password
 passwordInput = browser.find_element_by_id('password')
 passwordInput.send_keys(password)
-# click the login button
+# click the login button.
 loginButton = browser.find_element_by_id('loginbtn')
 loginButton.click()
-# Do more stuff
-# you might want to wait the new page to load
+# Get the links of all courses
+browser.implicitly_wait(5)
+# this works
+# course_list =  browser.find_elements_by_css_selector('li.type_course.depth_3')
+course_list =  browser.find_elements_by_css_selector('li.type_course.depth_3 > p > a')
+
+#if I click I lose my links. Or do I?
+course_list[0].click()
+browser.implicitly_wait(5)
+course_list[1].click()
